3b3568de378f900deeca776a1f48a255807dcc60
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-08-17 23:03:30 -0500

    Implement ability to insert subredddit post data when not available

--- app.py
+++ app.py
@@ -6,34 +6,38 @@ from praw.models import Submission as PrawSubmission
 from praw.models import Subreddit as PrawSubreddit
 
 from chalicelib import settings, utils
+from chalicelib.models import Subreddit
 
 app = Chalice(app_name='snoosdigest-updater')
 
 
-@app.lambda_function(name='reddit-posts')
+# @app.lambda_function(name='reddit-posts')
 def reddit_posts(event: dict, context: Any) -> dict[str, list]:
     print(f'IS_PROD_SYSTEM: {settings.IS_PROD_SYSTEM}')
     app_settings = settings.SYS_SETTINGS
 
     praw_reddit: PrawReddit = PrawReddit(**app_settings['REDDIT_APP_SETTINGS'])
 
-    subreddit_names = utils.get_subreddits_to_update()
-    res = {}
-    for subreddit_name in subreddit_names:
-        praw_subreddit: PrawSubreddit = praw_reddit.subreddit(subreddit_name)
+    subreddit_objs: list[Subreddit] = utils.get_subreddits_to_update()
+    res: dict = {}
+    for subreddit_obj in subreddit_objs:
+        if subreddit_obj.subreddit_id != 9:
+            continue
+        praw_subreddit: PrawSubreddit = praw_reddit.subreddit(subreddit_obj.display_name)
         subreddit_top_posts: Iterable[PrawSubmission] = praw_subreddit.top(
             'day', limit=settings.MAX_NUM_POSTS_PER_SUBREDDIT
         )
-        post_results = []
-        for post in subreddit_top_posts:
-            post_results.append(
-                {
-                    'reddit_id': post.id,
-                    'author_name': post.author.name if post.author else '',  # User is "deleted"
-                    'upvotes': post.score,
-                    'num_comments': post.num_comments,
-                }
+
+        for i, post in enumerate(subreddit_top_posts, 1):
+            current_subreddit_post = utils.get_subreddit_post(
+                subreddit_obj.subreddit_id, i, time_filter='day'
             )
-        res[subreddit_name] = post_results
+
+            if not current_subreddit_post:
+                utils.insert_subreddit_post(post, subreddit_obj.subreddit_id, i, time_filter='day')
+                continue
+
+            # Update existing subreddit
+            print(f'UPDATE {current_subreddit_post}')
 
     return res
--- chalicelib/utils.py
+++ chalicelib/utils.py
@@ -1,21 +1,116 @@
+from datetime import datetime, timezone
+
+from praw.models import Submission as PrawSubmission
 from praw.models import Subreddit as PrawSubreddit
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from chalicelib.models import PgSession, Subreddit
+from chalicelib.models import PgSession, Subreddit, SubredditPost
 
 
 def update_or_insert_subreddit_posts(praw_subreddit: PrawSubreddit, time_filter: str) -> list[dict]:
     pass
 
 
-def get_subreddits_to_update() -> list[str]:
-    statement = select(Subreddit.display_name)
+def get_subreddits_to_update() -> list[Subreddit]:
+    statement = select(Subreddit)
 
     pg_session: Session
     with PgSession.begin() as pg_session:
         result = pg_session.execute(statement).scalars().all()
-
-    print(result)
+        pg_session.expunge_all()  # To make row objects available outside the scope of the session
 
     return result
+
+
+def get_subreddit_post(subreddit_id: int, order: int, time_filter: str = 'day') -> str:
+    statement = select(SubredditPost).where(
+        SubredditPost.subreddit_id == subreddit_id,
+        SubredditPost.__table__.columns[f'top_{time_filter}_order'] == order,
+    )
+
+    pg_session: Session
+    with PgSession.begin() as pg_session:
+        result = pg_session.execute(statement).scalars().first()
+        pg_session.expunge_all()
+
+    return result
+
+
+def insert_subreddit_post(
+    subreddit_post: PrawSubmission, subreddit_id: int, order: int, time_filter: str = 'day'
+) -> None:
+    new_post = SubredditPost(
+        reddit_id=subreddit_post.id,
+        reddit_url=subreddit_post.shortlink,
+        title=subreddit_post.title,
+        body=get_subreddit_post_body(subreddit_post),
+        author_name=subreddit_post.author.name if subreddit_post.author else '',
+        upvotes=subreddit_post.score,
+        upvote_ratio=subreddit_post.upvote_ratio,
+        num_comments=subreddit_post.num_comments,
+        img_url=get_img_url(subreddit_post),
+        video_url=get_video_url(subreddit_post),
+        over_18=subreddit_post.over_18,
+        spoiler=subreddit_post.spoiler,
+        created_timestamp_utc=datetime.fromtimestamp(subreddit_post.created_utc),
+        created_unix_timestamp=subreddit_post.created_utc,
+        data_updated_timestamp_utc=datetime.now(tz=timezone.utc),
+        subreddit_id=subreddit_id,
+        **{f'top_{time_filter}_order': order},
+    )
+
+    pg_session: Session
+    with PgSession.begin() as pg_session:
+        pg_session.add(new_post)
+        pg_session.commit()
+
+
+def generate_full_reddit_link(link_path: str) -> str:
+    return f'https://www.reddit.com{link_path}'.strip()
+
+
+def generate_reddit_link_from_id(reddit_id: str) -> str:
+    return f'https://redd.it/{reddit_id}'.strip()
+
+
+def normalize_text_content(text: str) -> str:
+    text = text.replace('&#x200B;', '')
+    return text.strip()
+
+
+def get_img_url(subreddit_post: PrawSubmission) -> str:
+    if 'i.redd.it' in subreddit_post.url:
+        return subreddit_post.url
+    return ''
+
+
+def get_video_url(subreddit_post: PrawSubmission) -> str:
+    if 'v.redd.it' in subreddit_post.url and subreddit_post.media.get('reddit_video'):
+        return subreddit_post.media['reddit_video'].get('fallback_url', '')
+    return ''
+
+
+def get_reddit_permalink(subreddit_post: PrawSubmission) -> str:
+    return generate_full_reddit_link(subreddit_post.permalink)
+
+
+def get_subreddit_post_body(subreddit_post: PrawSubmission) -> str:
+    body: str = subreddit_post.selftext or ''
+
+    # Check if body is just a http link
+    permalink: str = get_reddit_permalink(subreddit_post)
+    if not body:
+        if subreddit_post.id_from_url(permalink) != subreddit_post.id:
+            # Link is another reddit post
+            body = permalink
+        elif hasattr(subreddit_post, 'url_overridden_by_dest'):
+            # Link is a non-reddit article
+            body = subreddit_post.url_overridden_by_dest
+
+    # Check if body is a cross-post (nests another reddit post directly in the post body)
+    if not body and hasattr(subreddit_post, 'crosspost_parent_list'):
+        crosspost_id: str = subreddit_post.crosspost_parent_list[0].get('id', '')
+        body = generate_reddit_link_from_id(crosspost_id)
+
+    return normalize_text_content(body)
