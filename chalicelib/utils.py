618088eae06bd47a6c414cf01a8439afb9832f4a
Peter Hu <44376800+PeterBohai@users.noreply.github.com>
2022-08-26 13:25:19 -0500

    Reduce number of fetches and add praw logs

--- chalicelib/utils.py
+++ chalicelib/utils.py
@@ -1,11 +1,9 @@
-import re
 import time
 from datetime import datetime, timezone
-from typing import Any
+from typing import Optional
 
-from praw.models import Redditor as PrawRedditor
 from praw.models import Submission as PrawSubmission
-from sqlalchemy import select, update
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from chalicelib.models import PgSession, Subreddit, SubredditPost
@@ -40,12 +38,15 @@ def insert_subreddit_post(
     subreddit_post: PrawSubmission, subreddit_id: int, order: int, time_filter: str = 'day'
 ) -> None:
     start_time = time.time()
+    post_id = subreddit_post.id
+    post_permalink = subreddit_post.permalink
+    post_selftext = subreddit_post.selftext
     new_post = SubredditPost(
-        reddit_id=subreddit_post.id,
+        reddit_id=post_id,
         reddit_url=subreddit_post.shortlink,
         title=subreddit_post.title,
-        body=get_subreddit_post_body(subreddit_post),
-        author_name=get_author_name(subreddit_post.author),
+        body=get_subreddit_post_body(subreddit_post, post_id, post_permalink, post_selftext),
+        author_name=subreddit_post.author.name,
         upvotes=subreddit_post.score,
         upvote_ratio=subreddit_post.upvote_ratio,
         num_comments=subreddit_post.num_comments,
@@ -66,38 +67,34 @@ def insert_subreddit_post(
     print(f'    --> {time.time() - start_time:.4f}s - Time taken to insert_subreddit_post')
 
 
-def update_subreddit_post(
-    subreddit_post: PrawSubmission, curr_post: SubredditPost, time_filter: str = 'day'
-) -> None:
+def update_subreddit_post(subreddit_post: PrawSubmission, curr_post: SubredditPost) -> None:
     start_time = time.time()
-    order_col = f'top_{time_filter}_order'
-    update_statement = (
-        update(SubredditPost)
-        .where(
-            SubredditPost.subreddit_id == curr_post.subreddit_id,
-            getattr(SubredditPost, order_col) == getattr(curr_post, order_col),
-        )
-        .values(
-            reddit_id=subreddit_post.id,
-            reddit_url=subreddit_post.shortlink,
-            title=subreddit_post.title,
-            body=get_subreddit_post_body(subreddit_post),
-            author_name=get_author_name(subreddit_post.author),
-            upvotes=subreddit_post.score,
-            upvote_ratio=subreddit_post.upvote_ratio,
-            num_comments=subreddit_post.num_comments,
-            img_url=get_img_url(subreddit_post),
-            video_url=get_video_url(subreddit_post),
-            over_18=subreddit_post.over_18,
-            spoiler=subreddit_post.spoiler,
-            created_timestamp_utc=datetime.fromtimestamp(subreddit_post.created_utc),
-            created_unix_timestamp=subreddit_post.created_utc,
-            data_updated_timestamp_utc=datetime.now(tz=timezone.utc),
-        )
-    )
+    post_id = subreddit_post.id
+    post_permalink = subreddit_post.permalink
+    post_selftext = subreddit_post.selftext
+
     pg_session: Session
-    with PgSession.begin() as pg_session:
-        pg_session.execute(update_statement)
+    with PgSession.begin():
+        curr_post.body = get_subreddit_post_body(
+            subreddit_post, post_id, post_permalink, post_selftext
+        )
+        curr_post.upvotes = subreddit_post.score
+        curr_post.upvote_ratio = subreddit_post.upvote_ratio
+        curr_post.num_comments = subreddit_post.num_comments
+        curr_post.img_url = (get_img_url(subreddit_post),)
+        curr_post.video_url = (get_video_url(subreddit_post),)
+        curr_post.over_18 = (subreddit_post.over_18,)
+        curr_post.spoiler = (subreddit_post.spoiler,)
+        if curr_post.reddit_id != post_id:
+            print("*New* Post update found current reddit_id != post_id")
+            curr_post.reddit_id = post_id
+            curr_post.reddit_url = subreddit_post.shortlink
+            curr_post.title = subreddit_post.title
+            curr_post.author_name = subreddit_post.author.name
+            curr_post.created_timestamp_utc = datetime.fromtimestamp(subreddit_post.created_utc)
+            curr_post.created_unix_timestamp = subreddit_post.created_utc
+
+        curr_post.data_updated_timestamp_utc = datetime.now(tz=timezone.utc)
 
     print(f'    --> {time.time() - start_time:.4f}s - Time taken to update_subreddit_post')
 
@@ -115,17 +112,10 @@ def normalize_text_content(text: str) -> str:
     return text.strip()
 
 
-def get_author_name(author_obj: PrawRedditor) -> str:
-    # Directly getting post.author.name means an additional request. Extract from repr instead.
-    if not author_obj:
-        return ''
-    m: Any = re.search("name='(.*)'", repr(author_obj))
-    return m.group(1)
-
-
 def get_img_url(subreddit_post: PrawSubmission) -> str:
-    if 'i.redd.it' in subreddit_post.url:
-        return subreddit_post.url
+    url = subreddit_post.url
+    if 'i.redd.it' in url:
+        return url
     return ''
 
 
@@ -135,17 +125,15 @@ def get_video_url(subreddit_post: PrawSubmission) -> str:
     return ''
 
 
-def get_reddit_permalink(subreddit_post: PrawSubmission) -> str:
-    return generate_full_reddit_link(subreddit_post.permalink)
-
-
-def get_subreddit_post_body(subreddit_post: PrawSubmission) -> str:
-    body: str = subreddit_post.selftext or ''
+def get_subreddit_post_body(
+    subreddit_post: PrawSubmission, post_id: str, post_permalink: str, post_selftext: Optional[str]
+) -> str:
+    body: str = post_selftext or ''
 
     # Check if body is just a http link
-    permalink: str = get_reddit_permalink(subreddit_post)
+    permalink: str = generate_full_reddit_link(post_permalink)
     if not body:
-        if subreddit_post.id_from_url(permalink) != subreddit_post.id:
+        if subreddit_post.id_from_url(permalink) != post_id:
             # Link is another reddit post
             body = permalink
         elif hasattr(subreddit_post, 'url_overridden_by_dest'):
